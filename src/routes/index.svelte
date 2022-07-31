bbed9f8f27e0b482ecda72f626bc2f0332268435
Marten <focccus@users.noreply.github.com>
2022-07-31 17:44:35 +0200

    clean up + styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,6 +4,7 @@
 
 	let searchUrl = '';
 	let searchUrlInput = '';
+	let blacklistInput = '';
 	let error = '';
 	let timer: any = undefined;
 	let interval = 15;
@@ -19,6 +20,7 @@
 
 		if (searchUrlInput.match(/https:\/\/www\.ebay-kleinanzeigen\.de\/.*/)) {
 			searchUrl = searchUrlInput;
+			items = [];
 			if (timer) clearInterval(timer);
 			timer = setInterval(lookup, interval * 1000);
 			lookup();
@@ -28,16 +30,16 @@
 	}
 
 	async function lookup() {
-		//let res = await fetch(searchUrl).then((res) => res.text());
-
-		//const dom = new JSDOM(res);
 		fetching = true;
 		error = '';
+		let blacklist: string[] = blacklistInput.trim().length ? blacklistInput.split(/\s/) : [];
 		const $ = (window as any).$;
 		$.get('https://api.allorigins.win/raw?url=' + searchUrl, (html: any) => {
 			$(html)
 				.find('.aditem')
 				.each(function (this: any) {
+					if ($(this).find('.icon-feature-topad').length) return;
+
 					let item: Item = {
 						url: $(this).attr('data-href'),
 						price: $(this).find('.aditem-main--middle--price').text().trim(),
@@ -61,16 +63,16 @@
 						.each(function (this: any) {
 							item.tags.push($(this).text());
 						});
-					// if (
-					// 	!config.blackList.some((b) =>
-					// 		(item.title + item.desc + item.location).toLowerCase().includes(b)
-					// 	)
-					// )
-					if (!items.some((it) => it.url == item.url)) {
+
+					if (
+						!items.some((it) => it.url == item.url) &&
+						!blacklist.some((b) =>
+							(item.title + item.desc + item.location).toLowerCase().includes(b)
+						)
+					) {
 						items = [item, ...items];
 					}
 				});
-			//console.log(items);
 			fetching = false;
 		});
 	}
@@ -95,7 +97,7 @@
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
       "
 			bind:value={searchUrlInput}
-			placeholder="Ebay Such URL"
+			placeholder="Kleinanzeigen Such URL"
 		/>
 		<span class="ml-2">Interval:</span>
 		<input
@@ -120,6 +122,26 @@
 		/>
 		<span>s</span>
 		<input
+			class="
+        form-control
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+		ml-3
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+      "
+			bind:value={blacklistInput}
+			placeholder="Blacklist (Leerzeichen trennen)"
+		/>
+		<input
 			type="submit"
 			value={timer ? 'Pause' : 'Start'}
 			class="inline-block ml-3 px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
@@ -143,19 +165,25 @@
 <div class="grid grid-cols-3 p-2">
 	{#each items as item}
 		<a href={'https://www.ebay-kleinanzeigen.de' + item.url} target="__blank">
-			<div class="grid grid-cols-2 m-3">
-				<div class="m-3">
-					<h2 class="text-xl font-bold">{item.title}</h2>
-					<p>
-						<span class="text-xl"> {item.price} </span>
-						{#each item.tags as tag}
-							<span class="text-right">{tag}</span>
-						{/each}
-					</p>
-					<p>{item.location}</p>
-					<p>{item.time}</p>
+			<div class="grid grid-cols-[1fr_2fr] m-3 rounded hover:bg-slate-50">
+				<div class="aspect-square rounded flex items-center justify-center bg-slate-100">
+					<img src={item.img} alt="" />
 				</div>
-				<img src={item.img} alt="" />
+				<div class="m-3 flex flex-col place-content-around">
+					<div>
+						<h2 class="text-xl font-bold">{item.title}</h2>
+						<p>
+							<span class="text-xl"> {item.price} </span>
+							{#each item.tags as tag}
+								<span class="text-right">{tag}</span>
+							{/each}
+						</p>
+					</div>
+					<div>
+						<p>{item.location}</p>
+						<p>{item.time}</p>
+					</div>
+				</div>
 			</div>
 		</a>
 	{/each}
